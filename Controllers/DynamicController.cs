ff11d21ed3e0258a735db6b762e1aef770e554f7
Bob <crowcoder@gmail.com>
2020-01-19 08:57:42 -0500

    Add project files.

---
+++ Controllers/DynamicController.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using System.Web.Http;
+using System.Web.Mvc;
+using HttpPostAttribute = System.Web.Mvc.HttpPostAttribute;
+
+namespace DynamicTest.Controllers
+{
+    public class DynamicController : ApiController
+    {
+        [HttpPost]
+        public JsonResult PostData([FromBody]dynamic model)
+        {
+            var jr = new JsonResult();               
+            jr.Data = model;
+            return jr;
+        }
+    }
+}
