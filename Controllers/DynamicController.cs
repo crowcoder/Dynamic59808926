8aefd9a534f1eabde4b7f097b6a76ed9488db385
Bob <crowcoder@gmail.com>
2020-01-19 09:03:48 -0500

    removed unnecessary attribute

--- Controllers/DynamicController.cs
+++ Controllers/DynamicController.cs
@@ -1,13 +1,11 @@
 ﻿using System.Diagnostics;
 using System.Web.Http;
 using System.Web.Mvc;
-using HttpPostAttribute = System.Web.Mvc.HttpPostAttribute;
 
 namespace DynamicTest.Controllers
 {
     public class DynamicController : ApiController
     {
-        [HttpPost]
         public JsonResult PostData([FromBody]dynamic model)
         {
             var jr = new JsonResult();               
