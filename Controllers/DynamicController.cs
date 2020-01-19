a39ab98a4f44d8ec71705d55764c612e06bbf53e
Bob <crowcoder@gmail.com>
2020-01-19 10:15:32 -0500

    comment

--- Controllers/DynamicController.cs
+++ Controllers/DynamicController.cs
@@ -1,13 +1,10 @@
-﻿using System.Diagnostics;
-using System.Web.Http;
+﻿using System.Web.Http;
 using System.Web.Mvc;
-using HttpPostAttribute = System.Web.Mvc.HttpPostAttribute;
 
 namespace DynamicTest.Controllers
 {
     public class DynamicController : ApiController
     {
-        [HttpPost]
         public JsonResult PostData([FromBody]dynamic model)
         {
             var jr = new JsonResult();               
